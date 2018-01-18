2e796fcb4d90e5265ffa6df7288b886a2d3d7d32
Yipee-ki-yay <mopoz07@gmail.com>
2018-01-18 11:21:47 +0200

    Header, Slider

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	$('.slider').slick({
+		infinite: true,
+		dots: true,
+		arrows: false,
+	});
+
+});
+
+// $(document).ready(function(){
+//   $('.slider').slick({
+
+//   });
+// });
