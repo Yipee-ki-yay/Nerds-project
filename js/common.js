cd670eb45417fc462d2c9087d77a4eed1315875c
Yipee-ki-yay <mopoz07@gmail.com>
2018-02-02 22:20:56 +0200

    add catalog.html and some js

--- js/common.js
+++ js/common.js
@@ -1,15 +1,23 @@
-var btnPopap = document.querySelector('.js-show-popap');
-var popap = document.querySelector('.contacts');
-var btnClose = document.querySelector('.contacts-close');
-var nameField = popap.querySelector('.contacts_name');
-var form = popap.querySelector('form');
-var pasField = popap.querySelector('.contacts_email');
-var textField = popap.querySelector('.contacts_text');
-var storage = localStorage.getItem("login");
+const btnPopap = document.querySelector('.js-show-popap');
+const popap = document.querySelector('.contacts');
+const btnClose = document.querySelector('.contacts-close');
+const nameField = popap.querySelector('.contacts_name');
+const form = popap.querySelector('form');
+const pasField = popap.querySelector('.contacts_email');
+const textField = popap.querySelector('.contacts_text');
+const storage = localStorage.getItem("login");
+const overlay = document.querySelector('.contacts-overlay');
+
+const shopItemsContainer = document.querySelector('.catalog-items');
+const shopItems = document.querySelectorAll('.catalog-item');
+const shopItemHover = document.querySelectorAll('.catalog-item-info');
+
+
 
 btnPopap.addEventListener("click", function(){
 	event.preventDefault();
 	popap.classList.add('contacts-show');
+	overlay.classList.add('show');
 	nameField.focus();
 
 	if (storage) {
@@ -22,6 +30,13 @@ btnClose.addEventListener("click", function() {
 	event.preventDefault();
 	popap.classList.remove('contacts-show');
 	popap.classList.remove('modal-error');
+	overlay.classList.remove('show');
+});
+
+overlay.addEventListener("click", function (){
+	popap.classList.remove('contacts-show');
+	popap.classList.remove('modal-error');
+	overlay.classList.remove('show');
 });
 
 window.addEventListener("keydown", function(event) {
@@ -29,6 +44,7 @@ window.addEventListener("keydown", function(event) {
 		if(popap.classList.contains('contacts-show')) {
 			popap.classList.remove('contacts-show');
 			popap.classList.remove('modal-error');
+			overlay.classList.remove('show');
 		}
 	}
 });
@@ -42,6 +58,32 @@ form.addEventListener("submit", function(){
 	}
 });
 
+//====================================================================== catalog-item hover effect begin
+
+// document.onmouseover = function (event) {
+// 	let target = event.target;
+// 	console.log(event.target);	
+// 	if (target.className == "catalog-item-head" || target.className == "catalog-item-image") {
+// 		let hideItem = target.parentElement.getElementsByClassName('catalog-item-info');
+// 		hideItem[0].classList.add('show');
+// 	}	 
+	// else if (target.className != "catalog-item-head" || target.className != "catalog-item-image" || target.className != "catalog-item-info") {
+	// 	for (let i = 0; i < shopItemHover.length; i++) {
+	// 		shopItemHover[i].classList.remove('show');
+	// 	}
+	// }
+//}
+
+// shopItems.addEventListener("mouseover", function(){
+// 	shopItemHover.classList.add('show');
+// });
+// shopItem.addEventListener("mouseout", function(){
+// 	shopItemHover.classList.remove('show');
+// });
+
+//====================================================================== catalog-item hover effect begin
+
+//====================== Slider BEGIN
 
 $(function(){
 
@@ -51,12 +93,6 @@ $(function(){
 		arrows: false,
 	});
 
+//====================== Slider END
+
 });
-
-
-
-// $(document).ready(function(){
-//   $('.slider').slick({
-
-//   });
-// });
